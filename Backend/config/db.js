43a1bb22c605480de171fab6e656c125806a55f7
betterside <bettersideserver@gmail.com>
2026-04-09 13:00:27 +0530

    Update db.js

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -3,10 +3,10 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
-    console.log("MongoDB Atlas Connected ✅");
+    console.log("MongoDB Connected ✅");
   } catch (error) {
-    console.error("MongoDB connection failed");
-    console.error(error);
+    console.error("DB Error ❌", error.message);
+    process.exit(1);
   }
 };
 
